af0fda1d99cafc247f9460103cb1e971418eee42
Anish.Joshi <anish.joshi@cedargate.com>
2024-02-07 08:26:09 +0545

    updated ecommerce

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,295 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { PayPalButton } from "react-paypal-button-v2";
+import { Link } from "react-router-dom";
+import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import {
+  getOrderDetails,
+  payOrder,
+  deliverOrder,
+} from "../actions/orderActions";
+import {
+  ORDER_PAY_RESET,
+  ORDER_DELIVER_RESET,
+  ORDER_PAY_SUCCESS,
+} from "../constants/orderConstants";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+
+const OrderScreen = ({ match, history }) => {
+  const orderId = match.params.id;
+
+  const data = useLocation();
+  const params = queryString.parse(data.search);
+  const { oid, amt, refId, fail } = params;
+  console.log("Esewa Data:::", data);
+  let successPay = false;
+  if (oid?.length > 0 && amt?.length > 0 && refId?.length > 0) {
+    successPay = true;
+  }
+
+  const [sdkReady, setSdkReady] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
+
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, success: successPays } = orderPay;
+
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  if (!loading) {
+    //   Calculate prices
+    const addDecimals = (num) => {
+      return (Math.round(num * 100) / 100).toFixed(2);
+    };
+
+    order.itemsPrice = addDecimals(
+      order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    );
+  }
+
+  const cart = useSelector((state) => state.cart);
+  const { paymentMethod } = cart;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    }
+    if (successPay) {
+      successPaymentHandler({
+        id: refId,
+        status: oid,
+        update_time: Date.now(),
+        payer: { email_address: userInfo.email },
+      });
+    }
+    if (
+      !order ||
+      // || successPay || successDeliver
+      order._id !== orderId
+    ) {
+      dispatch({ type: ORDER_PAY_RESET });
+      dispatch({ type: ORDER_DELIVER_RESET });
+      dispatch(getOrderDetails(orderId));
+    } else if (!order.isPaid) {
+      setSdkReady(true);
+    }
+  }, [dispatch, orderId, successPay, successDeliver, order]);
+
+  const successPaymentHandler = (paymentResult) => {
+    console.log(paymentResult);
+    dispatch(payOrder(orderId, paymentResult));
+    history.push(data.pathname);
+  };
+
+  const deliverHandler = () => {
+    dispatch(deliverOrder(order));
+  };
+
+  console.log(order);
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong>{" "}
+                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+              </p>
+              <p>
+                <strong>Address:</strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.length === 0 ? (
+                <Message>Order is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x Rs{item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>Rs {order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>Rs {order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>Rs {order.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>Rs {order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+                  {!sdkReady ? (
+                    <Loader />
+                  ) : (
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <form
+                        action="https://uat.esewa.com.np/epay/main"
+                        method="POST"
+                      >
+                        <input
+                          value={order.totalPrice}
+                          name="tAmt"
+                          type="hidden"
+                        />
+                        <input
+                          value={order.itemsPrice}
+                          name="amt"
+                          type="hidden"
+                        />
+                        <input
+                          value={order.taxPrice}
+                          name="txAmt"
+                          type="hidden"
+                        />
+                        <input value="0" name="psc" type="hidden" />
+                        <input value="0" name="pdc" type="hidden" />
+                        <input value="EPAYTEST" name="scd" type="hidden" />
+                        <input value={order._id} name="pid" type="hidden" />
+                        <input
+                          value={window.location.href}
+                          type="hidden"
+                          name="su"
+                        />
+                        <input
+                          value={window.location.href + "?fail=true"}
+                          type="hidden"
+                          name="fu"
+                        />
+                        <button type="submit" style={{ border: "none" }}>
+                          <img
+                            src="https://i2.wp.com/insidergeeks.com/wp-content/uploads/2020/09/eSewa-Digital-Wallet-Nepal.jpeg"
+                            className="card-img-top"
+                            style={{
+                              width: "100px",
+                              height: "50px",
+                              borderRadius: "5px",
+                            }}
+                          />
+                        </button>
+                      </form>
+                    </div>
+                  )}
+                </ListGroup.Item>
+              )}
+              {loadingDeliver && <Loader />}
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type="button"
+                      className="btn btn-block"
+                      onClick={deliverHandler}
+                    >
+                      Mark As Delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderScreen;
